047963d3f066bef9c093e091972e4e6396d5cfd7
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2024-12-17 16:23:34 +0100

    nextjsconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['firebasestorage.googleapis.com'], // List Firebase Storage domain
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/**', // Updated pathname for Firebase Storage v0 compatibility
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
